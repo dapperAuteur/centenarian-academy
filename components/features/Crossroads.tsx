2a5ab388075fe9b502139ec402103933fe86319c
awe <aweful@awews.com>
2026-01-30 13:38:48 -0500

    resolve ide warning for unused value

--- components/features/Crossroads.tsx
+++ components/features/Crossroads.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /** File Path: ./centenarian-academy/components/features/Crossroads.tsx */
 'use client'
